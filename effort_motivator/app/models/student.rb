452f3839fa5951da6c8479b7b72abd0791f83661
Connie Yao <connie.yao.2010@gmail.com>
2012-06-07 23:17:09 -0400

    Passwords can be entered in db without it hating me.  yay?

--- effort_motivator/app/models/student.rb
+++ effort_motivator/app/models/student.rb
@@ -1,7 +1,13 @@
 class Student < ActiveRecord::Base
   belongs_to :group
 
+
+  has_secure_password
+
   before_save :create_remember_token
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   private 
   
---
+++ effort_motivator/db/migrate/20120608020814_add_password_digest_to_students.rb
@@ -0,0 +1,6 @@
+class AddPasswordDigestToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :password_digest, :string
+
+  end
+end
