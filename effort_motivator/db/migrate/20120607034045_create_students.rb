d91ac4291fce4bd19cc6649efb491308752ec26b
Connie Yao <connie.yao.2010@gmail.com>
2012-06-07 21:50:00 -0400

    please work

--- effort_motivator/db/migrate/20120607034045_create_students.rb
+++ effort_motivator/db/migrate/20120607034045_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
-      t.string :password
       t.string :pic
       t.text :profile
       t.integer :coins
--- effort_motivator/db/schema.rb
+++ effort_motivator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607203426) do
+ActiveRecord::Schema.define(:version => 20120607041457) do
 
   create_table "games", :force => true do |t|
     t.string   "name"
@@ -36,10 +36,9 @@ ActiveRecord::Schema.define(:version => 20120607203426) do
     t.text     "profile"
     t.integer  "coins"
     t.integer  "group_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "remember_token"
-    t.string   "password_digest"
   end
 
   add_index "students", ["remember_token"], :name => "index_students_on_remember_token"
